a08cbe65d334d72391a5d13a34be086a795f774f
hiramekun <thescript1210@gmail.com>
2018-05-05 21:19:05 +0900

    Fix plot

--- machine_learning/linear_discriminant_analysis.R
+++ machine_learning/linear_discriminant_analysis.R
@@ -1,30 +1,24 @@
 library(mvtnorm)
-sigma = matrix(c(1, 1, 1, 9), ncol = 2)
-rand = rmvnorm(n = 1000, mean = c(2, 0), sigma)
-rand = rmvnorm(n = 1000, mean = c(-2, 0), sigma)
-x1 = rand[, 1]
-x2 = rand[, 2]
-plot(x1, x2)
 
-x1 = seq(-3, 3, length = 50)
-x2 = x1
-f = function(x1, x2, mean) {
-  dmvnorm(matrix(c(x1, x2), ncol = 2), mean = mean, sigma = sigma)
-}
+beta = pi / 4
+sigma = matrix(c(
+  sd1,
+  8 * cos(beta) * sin(beta),
+  8 * cos(beta) * sin(beta),
+  sd2
+), ncol = 2)
+
 mu1 = c(2, 0)
 mu2 = c(-2, 0)
 
-alpha = 0.1
+alpha = 0.5
 n = 600
 n1 = sum(runif(600) < alpha)
 n2 = n - n1
-x1 = c(rnorm(n1, mean = 2), 1 * rnorm(n1))
-x2 = c(rnorm(n2, mean = -2), 3 * rnorm(n2))
 
 a = solve(sigma) %*% (mu1 - mu2)
 b = -1 / 2 * (t(mu1) %*% solve(sigma) %*% mu1 - t(mu2) %*% solve(sigma) %*% mu2) + log(n1 /
                                                                                          n2)
-
 linear = function(x) {
   if (a[2] == 0) {
     rep(0, length(x))
@@ -32,19 +26,23 @@ linear = function(x) {
     -(a[1] / a[2]) * x + rep(b / a[2], length(x))
   }
 }
+
+rand1 = rmvnorm(n = n1, mean = c(0, 2), sigma)
+rand2 = rmvnorm(n = n2, mean = c(0, -2), sigma)
+
 plot(
-  rnorm(n1, mean = 2),
-  3 * rnorm(n1),
-  xlim = c(-6, 6),
+  rand1[, 1],
+  rand1[, 2],
+  xlim = c(-10, 10),
   ylim = c(-10, 10),
   col = "blue",
   pch = 20
 )
 par(new = T)
 plot(
-  rnorm(n2, mean = -2),
-  3 * rnorm(n2),
-  xlim = c(-6, 6),
+  rand2[, 1],
+  rand2[, 2],
+  xlim = c(-10, 10),
   ylim = c(-10, 10),
   col = "red",
   pch = 20
